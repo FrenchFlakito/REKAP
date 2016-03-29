f3f5edd9fdd34f6d9fa6b1e2f1af2f4882fb97a0
frenchflakito <guillaume01.simplon@gmail.com>
2016-03-29 20:54:19 +0200

    add stock retrieve, basic and static for now

---
+++ stock.py
@@ -0,0 +1,20 @@
+import requests
+
+def get_stock_history():
+    url = "http://marketdata.websol.barchart.com/getHistory.json?key=4b3c254703f6e53f6028a8d430ec0ee2&symbol=IBM&type=daily&startDate=20160321000000"
+    try:
+        hist_request = requests.get(url)
+        json_history = hist_request.json()
+    except:
+        json_history["message"] = "Error in API access"
+    print "json_history", json_history
+    return json_history
+
+def stock_display():
+    history = get_stock_history()
+    for i in history["results"]:
+        print i["timestamp"][0:9], "closing value :" i["close"]
+
+    print "******* DONE *******"
+
+stock_display()
