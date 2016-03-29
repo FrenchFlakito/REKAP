0beae40ada245cbc7ddc8a4cdf47ec4300e984e6
frenchflakito <guillaume01.simplon@gmail.com>
2016-03-29 22:44:43 +0200

    stock improvement

--- stock.py
+++ stock.py
@@ -1,23 +1,23 @@
 import requests
 
-def get_stock_history():
-    url = "http://marketdata.websol.barchart.com/getHistory.json?key=4b3c254703f6e53f6028a8d430ec0ee2&symbol=IBM&type=daily&startDate=20160321000000"
+def get_stock_history(stock):
+    params = {"symbol": stock, "type": "daily", "startDate":"20160321000000"}
+    url = "http://marketdata.websol.barchart.com/getHistory.json?key=4b3c254703f6e53f6028a8d430ec0ee2"
     try:
-        hist_request = requests.get(url)
-        json_history = hist_request.json()
+        # with requests.get we get a Response object
+        r = requests.get(url, params)
+        print "retrieving from", r.url
+        json_hist = r.json()
     except:
-        json_history["message"] = "Error in API access"
-    print "json_history", json_history
-    return json_history
+        json_hist["message"] = "Error in API access"
+    return json_hist
 
-def stock_display():
-    history = get_stock_history()
+def stock_display(stock):
     message = ""
-    message = message + "\nIBM :\n"
+    history = get_stock_history(stock)
     for i in history["results"]:
-        message = message + i["timestamp"][0:9] + " closing value : " + str(i["close"]) + "\n"
-    message = message + "-------------\n"
-    print message
+        message = message + i["timestamp"][0:10] + " closing value : " + str(i["close"]) + "\n"
+    message = "\n" + stock + " :\n" + message + "-------------\n"
     return message
 
-stock_display()
+print stock_display("TSLA")
